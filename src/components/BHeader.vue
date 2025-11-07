373ba68635bca441e174cf780be5dfda642a76a1
Kashmain Tahir <ktah0006@student.monash.edu>
2025-11-07 23:40:28 +0700

    navbar and routing complete - no reponsive design

--- src/components/BHeader.vue
+++ src/components/BHeader.vue
@@ -2,52 +2,39 @@
   <!-- Using Bootstrap's Header template (starter code) -->
   <!-- https://getbootstrap.com/docs/5.0/examples/headers/ -->
   <div class="container">
-    <header class="d-flex justify-content-center py-3">
+    <header class="d-flex justify-content-center py-3" role="banner">
       <ul class="nav nav-pills">
-        <li class="nav-item">
-          <router-link to="/member-login" class="nav-link" active-class="active" aria-current="page"
+        <li class="nav-item mx-2">
+          <router-link to="/member-login" class="button" active-class="active"
             >Member Login</router-link
           >
         </li>
 
-        <li class="nav-item">
-          <router-link to="/admin-login" class="nav-link" active-class="active" aria-current="page"
+        <li class="nav-item mx-2">
+          <router-link to="/admin-login" class="button" active-class="active"
             >Admin Login</router-link
           >
         </li>
-
-        <li class="nav-item">
-          <router-link
-            to="/community-feed"
-            class="nav-link"
-            active-class="active"
-            aria-current="page"
+        <li class="nav-item mx-2">
+          <router-link to="/community-feed" class="button" active-class="active"
             >Community Feed</router-link
           >
         </li>
 
-        <li class="nav-item">
-          <router-link
-            to="/feed-analytics"
-            class="nav-link"
-            active-class="active"
-            aria-current="page"
+        <li class="nav-item mx-2">
+          <router-link to="/feed-analytics" class="button" active-class="active"
             >Feed Analytics</router-link
           >
         </li>
 
-        <li class="nav-item">
-          <router-link
-            to="/member-signup"
-            class="nav-link"
-            active-class="active"
-            aria-current="page"
+        <li class="nav-item mx-2">
+          <router-link to="/member-signup" class="button" active-class="active"
             >Member Sign Up</router-link
           >
         </li>
 
-        <li class="nav-item">
-          <router-link to="/user-manager" class="nav-link" active-class="active" aria-current="page"
+        <li class="nav-item mx-2">
+          <router-link to="/user-manager" class="button" active-class="active"
             >User Manager</router-link
           >
         </li>
